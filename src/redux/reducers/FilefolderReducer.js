1679e750cce18d987fc43de07ae68290be8ce051
Developer Sid <142804820+Siddarth-abcs@users.noreply.github.com>
2023-10-08 21:26:31 +0530

    Update FilefolderReducer.js

--- src/redux/reducers/FilefolderReducer.js
+++ src/redux/reducers/FilefolderReducer.js
@@ -1,28 +1,49 @@
-import * as types from "../actionsTypes/authActionTypes"
-
+import * as types from '../actionsTypes/filefolderactiontype';
 
 const initialState = {
-  isAuthenticated: false,
-  user: {}
-};
-
-const authReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case types.SIGN_IN:
-      return{
-        ...state,
-        isAuthenticated: true,
-        user: action.payload,
-      };
-      case types.SIGN_OUT:
-        return {
-          ...state,
-          isAuthenticated: false,
-          user: {},
-        };
-    default:
-      return state;
-}
+    isLoding: true,
+    currentFolder: "root",
+    userFolders: [],
+    userFiles: [],
+    adminFolders: [],
+    adminFiles: [],
 }
 
-export default authReducer;
+const fileFolderReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case types.CREATE_FOLDER:
+            return {
+                ...state,
+                userFolders: [...state.userFolders, action.payload],
+            };
+        case types.ADD_FOLDERS:
+            return {
+                ...state,
+                userFolders: action.payload,
+            };
+        case types.SET_LOADING:
+            return {
+                ...state,
+                isLoding: action.payload,
+            };
+        case types.CHANGE_FOLDER:
+            return {
+                ...state,
+                currentFolder: action.payload,
+            }
+        case types.ADD_FILES:
+            return {
+                ...state,
+                userFiles: action.payload
+            }
+        case types.CREATE_FILE:
+            return {
+                ...state,
+                userFiles: [...state.userFiles,action.payload]
+            }
+        default:
+            return state;
+    }
+}
+
+export default fileFolderReducer;
